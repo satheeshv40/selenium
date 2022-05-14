b016a2cfb173addc9036e524136d42f50ed9423b
satheeshv40 <satheeshv40@yahoo.com>
2022-05-14 22:55:22 +0530

    commit1

---
+++ src/pack1/Test1.java
@@ -0,0 +1,22 @@
+package pack1;
+
+public class Test1 {
+
+	static int age=10;
+	int age1=11;
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Test1 t1= new Test1();
+
+		System.out.println(t1.age1);
+		
+	}
+	public void method1() {
+		System.out.println(age1);
+		System.out.println(age);
+		
+	}
+
+}
---
+++ src/pack1/Test2.java
@@ -0,0 +1,20 @@
+package pack1;
+
+import org.testng.annotations.Test;
+
+public class Test2 {
+	
+	
+	@Test
+	public void newmet() {
+	System.out.println("newmet");	
+	}
+	
+	@Test
+	public  void newmeta() {
+		System.out.println("newmeta");
+		
+	}
+	
+
+}

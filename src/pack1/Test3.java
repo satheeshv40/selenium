56aeab0c04f94073c71502a56c710fdb7b08e842
satheeshv40 <satheeshv40@yahoo.com>
2022-05-15 09:49:32 +0530

    devloped changes

---
+++ src/pack1/Test3.java
@@ -0,0 +1,26 @@
+package pack1;
+
+import org.testng.annotations.Test;
+
+public class Test3 {
+	
+	
+	@Test
+	public void newmet() {
+	System.out.println("newmet");	
+	}
+	
+	@Test
+	public  void newmeta() {
+		System.out.println("newmeta");
+		
+	}
+	
+	@Test
+	public  void newmeta1() {
+		System.out.println("newmeta1");
+		
+	}
+	
+
+}
